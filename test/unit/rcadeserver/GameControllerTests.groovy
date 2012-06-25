27d3dc358db85f21297e01a4463b6ffc07552a68
John Kelly <john.kelly@readytalk.com>
2012-06-25 16:22:19 -0600

    Add unit tests for GameController functions. (Incomplete)
    Index function for  GameController has passing tests for all 4 main
    use cases. Not all execution paths are accounted for. List and Create
    have coincidentally passing tests.

--- test/unit/rcadeserver/GameControllerTests.groovy
+++ test/unit/rcadeserver/GameControllerTests.groovy
@@ -8,7 +8,7 @@ import grails.test.mixin.*
 @TestFor(GameController)
 @Mock(Game)
 class GameControllerTests {
-
+	
 	protected void setup(){
 		super.setup()
 		controller = new GameController()
@@ -22,14 +22,15 @@ class GameControllerTests {
     }
 
     void testIndex_Post() {
-		
 		// Mock an http POST request
 		request.method = "POST"
-		// With a mock Game object in the params
+		// With a mock Game dictionary in the params
+		params.put("game", [romName: "T", gameName: "Test"])
+		mockDomain(Game)
 		// Mock nav to index for Game domain
         controller.index()
-		// We expect to be bounced to xmlList action by index@POST
-        assert response.redirectedUrl == "/game/xmlList"
+		// We expect a couple item properties
+		assert response.status == 201
     }
 	
 	void testIndex_Get_Anonymous() {	
@@ -48,25 +49,58 @@ class GameControllerTests {
 		// Mock nav to index for Game domain
 		controller.index()
 		// We expect to be bounced to xmlList action by index@GET
-		assert response.redirectedUrl == "/game/xmlShow/?romName=" + params.romName
+		assert response.redirectedUrl == "/game/xmlShow?romName=" + params.romName
 	}
-		
-	void textIndex_Put() {
-		// Mock an http GET request
-		request.method = "GET"
+	
+	void testIndex_Put() {
+		// Mock an http PUT request
+		request.method = "PUT"
+		// With a mock Game dictionary and Game name in the params
+		def g = new Game([romName: "a", gameName: "Alpha"])
+		mockDomain(Game)
+		g.save()
+		params.romName = "a"
+		params.game = [romName: "T", gameName: "Test"]
 		// Mock nav to index for Game domain
 		controller.index()
-		// We expect to be bounced to xmlList action by index@GET
-		assert response.redirectedUrl == "/game/xmlList"
+		// We expect to receive status 200
+		assert response.status == 200
 	}
 		
 	void testIndex_Delete() {
 		// Mock an http GET request
-		request.method = "GET"
+		request.method = "DELETE"
+		
+		// With a mock Game in memory, and its romName in params
+		def g = new Game([romName: "a", gameName: "Alpha"])
+		mockDomain(Game)
+		g.save()
+		params.romName = "a"
 		// Mock nav to index for Game domain
 		controller.index()
-		// We expect to be bounced to xmlList action by index@GET
-		assert response.redirectedUrl == "/game/xmlList"
+		// We expect to be informed of a successful delete
+		assert response.text == "Successfully Deleted."
+		assert response.status == 200
+		
+		response.reset()
+		
+		// Attempt with unfindable romName
+		params.romName = "b"
+		// Mock nav to index for Game domain
+		controller.index()
+		// We expect to be informed of an unfindable rom
+		assert response.text == params.romName + " not found."
+		assert response.status == 404
+		
+		response.reset()
+		
+		// Attempt with no romName
+		params.romName = null
+		// Mock nav to index for Game domain
+		controller.index()
+		// We expect to be informed of an unfindable rom
+		assert response.text == "DELETE request must include the romName\nExample: /rest/game/romName"
+		assert response.status == 400
     }
 
     void testList() {
